82d1ae14eaf3fe62ee08d798e7e41c8e0bfd7018
ali kamran <ali.kamran.98.98@gmail.com>
2023-06-20 16:17:16 +0330

    Initial commit

---
+++ app/src/main/java/com/example/stopwatchdemo/StopWatchService.kt
@@ -0,0 +1,42 @@
+package com.example.stopwatchdemo
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+import java.util.Timer
+import java.util.TimerTask
+import kotlin.math.roundToInt
+
+class StopWatchService : Service() {
+    override fun onBind(p0: Intent?): IBinder? = null
+
+    private val timer = Timer()
+
+    override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int {
+        val time = intent.getDoubleExtra(CURRENT_TIME, 0.0)
+        timer.scheduleAtFixedRate(StopWatchTimerClass(time), 0, 1000)
+        return START_NOT_STICKY
+    }
+
+    override fun onDestroy() {
+        timer.cancel()
+        super.onDestroy()
+    }
+
+    companion object {
+        const val CURRENT_TIME = "current time"
+        const val UPDATE_TIME = "update time"
+    }
+
+    private inner class StopWatchTimerClass(private var time: Double) : TimerTask() {
+        override fun run() {
+            val intent = Intent(UPDATE_TIME)
+            time++
+            intent.putExtra(CURRENT_TIME, time)
+            sendBroadcast(intent)
+        }
+
+
+
+    }
+}
